213e1ad90d70fe48869eb94e8d20ed87a9d74c03
ajaysingh8462 <ajaysingh84625@gmail.com>
2023-01-16 08:34:02 +0530

    [Ajay]-Creating database using ado.net

---
+++ Employee_Payroll/Data_Base.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll
+{
+    public class Data_Base
+    {
+        public void CreateDatabass()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-P65PD0V;Initial catalog=master;Integrated Security=true");
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("Create database Employee_Payroll", connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Databass ctearted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Employee_Payroll/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Data_Base data= new Data_Base();
+            data.CreateDatabass();
+        }
+    }
+}
